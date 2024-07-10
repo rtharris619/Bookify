8a9324325bff0c08c57002b8b782dce0835eb97c
Ryan Harris <ryan@synthesis.co.za>
2024-07-10 17:55:44 +0200

    App layer in progress

---
+++ Bookify.Domain/Apartments/ApartmentErrors.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments;
+
+public static class ApartmentErrors
+{
+    public static Error NotFound = new("Apartment.Found", "The apartment with the specified identifier was not found");
+}
---
+++ Bookify.Domain/Users/UserErrors.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public static class UserErrors
+{
+    public static Error NotFound = new("User.Found", "The user with the specified identifier was not found");
+}
