f8c098a92cf2fdb27c7e119507dbd452b2c39593
Ryan Harris <ryan@synthesis.co.za>
2024-07-11 19:34:16 +0200

    Started with Infra layer

---
+++ Bookify.Infrastructure/Configurations/BookingConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Bookings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+    }
+}
---
+++ Bookify.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(200)
+            .HasConversion(firstName => firstName.Value, value => new FirstName(value));
+
+        builder.Property(u => u.LastName)
+            .HasMaxLength(200)
+            .HasConversion(lastName => lastName.Value, value => new LastName(value));
+
+        builder.Property(u => u.Email)
+            .HasMaxLength(400)
+            .HasConversion(email => email.Value, value => new Domain.Users.Email(value));
+
+        builder.HasIndex(u => u.Email).IsUnique();
+    }
+}
